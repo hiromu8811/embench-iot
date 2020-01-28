a547cf768b32dfc69c074c2facde4d79eb004326
hiromu8811 <miyazaki@arch.cs.titech.ac.jp>
2020-01-28 17:21:55 +0900

    update config

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 rm -r bd
 mkdir bd
-python3 build_all.py --arch riscv32 --chip size-test-gcc --board ri5cyverilator
-#python3 build_all.py --arch riscv32 --chip rvcorep --board verilator
+#python3 build_all.py --arch riscv32 --chip size-test-gcc --board ri5cyverilator
+python3 build_all.py --arch riscv32 --chip rvcorep --board verilator
