acdf83981b9253dfc1f7a0f78c6e44543804ae37
hiromu8811 <miyazaki@arch.cs.titech.ac.jp>
2020-01-30 10:09:38 +0900

    add run.sh

---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+rm -f runlog.txt
+
+benchmark=(
+aha-mont64
+crc32
+cubic
+edn
+huffbench
+matmult-int
+minver
+nbody
+nettle-aes
+nettle-sha256
+nsichneu
+picojpeg
+qrduino
+sglib-combined
+slre
+st
+statemate
+ud
+wikisort
+)
+
+echo "run 19 benchmarks" > runlog.txt
+for bench in "${benchmark[@]}"
+do
+    echo "run ${bench}" >> runlog.txt
+    time simrv -a -m bd/bin/${bench}.bin -e 1G &>> runlog.txt
+done
