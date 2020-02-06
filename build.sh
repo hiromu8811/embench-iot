e57a94b306599a0f417f293cf75d1a44753784c3
hiromu8811 <miyazaki@arch.cs.titech.ac.jp>
2020-02-06 20:30:09 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -37,6 +37,11 @@ for bench in "${benchmark[@]}"
 do
     # *.bin
     ${RISCVDIR}/riscv32-unknown-elf-objcopy -O binary bd/src/${bench}/${bench} bd/bin/${bench}.bin
+    # padding
+    cp bd/bin/${bench}.bin bd/bin/_${bench}.bin
+    dd if=/dev/zero bs=64k count=1 >> bd/bin/_${bench}.bin
+    dd if=bd/bin/_${bench}.bin bs=64k count=1 > bd/bin/${bench}.bin
+    rm -f bd/bin/_${bench}.bin
     # *.mem
     hexdump -v -e '1/4 "%08x" "\n"' bd/bin/${bench}.bin > bd/mem/${bench}.mem
     # *.dump
